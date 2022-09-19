b49e95d32ac8a4a1335b6e6cce3cdcaf6c636138
Jonath-z <jonathanzihindula95@gmail.com>
2022-09-19 11:46:31 +0200

    new challenge added

---
+++ 19-sept-2022/Find the missing letter/index.js
@@ -0,0 +1,44 @@
+function findMissingLetter(array) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const firstLetter = array[0];
+  const lastLetter = array[array.length - 1];
+  const fullInterval = [];
+
+  const isUpperCase = () => {
+    return firstLetter === firstLetter.toUpperCase();
+  };
+
+  for (let letter of isUpperCase() ? alphabet.toUpperCase() : alphabet) {
+    if (isUpperCase()) {
+      if (letter === firstLetter) {
+        fullInterval.push(firstLetter);
+      } else if (
+        fullInterval[0] === firstLetter &&
+        alphabet.toUpperCase().indexOf(lastLetter) >
+          alphabet.toUpperCase().indexOf(firstLetter) &&
+        letter !== lastLetter
+      ) {
+        fullInterval.push(letter);
+      } else if (letter === lastLetter) {
+        fullInterval.push(lastLetter);
+        break;
+      }
+    } else {
+      if (letter === firstLetter) {
+        fullInterval.push(firstLetter);
+      } else if (
+        fullInterval[0] === firstLetter &&
+        alphabet.indexOf(lastLetter) > alphabet.indexOf(firstLetter) &&
+        letter !== lastLetter
+      ) {
+        fullInterval.push(letter);
+      } else if (letter === lastLetter) {
+        fullInterval.push(lastLetter);
+        break;
+      }
+    }
+  }
+
+  return fullInterval.filter((letter) => array.indexOf(letter) === -1)[0];
+}
---
+++ 19-sept-2022/Find the missing letter/index.test.js
@@ -0,0 +1,6 @@
+describe("KataTests", function () {
+  it("exampleTests", function () {
+    Test.assertEquals(findMissingLetter(["a", "b", "c", "d", "f"]), "e");
+    Test.assertEquals(findMissingLetter(["O", "Q", "R", "S"]), "P");
+  });
+});
