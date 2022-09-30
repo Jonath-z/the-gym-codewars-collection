0f6334f3e6e4b4203f4823672d30a9bee6a6d1ed
Jonath-z <jonathanzihindula95@gmail.com>
2022-09-30 08:38:00 +0200

    new Kata added

---
+++ 30-sept-2022/Replace With Alphabet Position/index.js
@@ -0,0 +1,44 @@
+function alphabetPosition(text) {
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  const lettersSet = text.split("");
+
+  let lettersPositionSet = [];
+
+  lettersSet.forEach((letter) => {
+    if (alphabet.includes(letter.toUpperCase())) {
+      let position =
+        alphabet.findIndex((el) => el === letter.toUpperCase()) + 1;
+      lettersPositionSet.push(position);
+    }
+  });
+
+  return lettersPositionSet.join(" ");
+}
