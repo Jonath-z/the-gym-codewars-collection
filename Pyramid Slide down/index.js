b2f50156a26827a8d34836c05774d74029853f0a
Jonath-z <jonathanzihindula95@gmail.com>
2022-12-12 11:14:01 +0200

    slide down pyramid added

---
+++ Pyramid Slide down/index.js
@@ -0,0 +1,9 @@
+function longestSlideDown(pyramid) {
+  for (let i = pyramid.length - 1; i > 0; i--) {
+    for (let j = 0; j < pyramid[i].length - 1; j++) {
+      pyramid[i - 1][j] += Math.max(pyramid[i][j], pyramid[i][j + 1]);
+    }
+  }
+
+  return pyramid[0][0];
+}
